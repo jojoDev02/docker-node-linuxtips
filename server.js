fcec1127343c020f860f2af3ae7a67ee46a3fa84
Jordana Cavalcante <88212453+jojoDev02@users.noreply.github.com>
2023-07-26 11:45:40 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('jojo do DevOps testando aqui '-'');
+  res.send('jojo do DevOps testando aqui');
 });
 
 app.get('*', (req, res) => {
